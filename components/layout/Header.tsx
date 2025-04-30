ab451ab455487912829de2ce2b45f605de8c9584
lupamo <arnoldlupamo@gmail.com>
2025-04-30 12:51:22 +0300

    partners info

--- components/layout/Header.tsx
+++ components/layout/Header.tsx
@@ -8,6 +8,7 @@ import google from '../../public/images/Google-Podcast.png'
 import youtube from '../../public/images/Youtube.png'
 import Button from '../ul/Buttons'
 import EpisodeList from '../podcast/EpisodeList'
+import Insipiration from './Inspiration'
 
 const Header = () => {
 	const [isVisible, setIsVisible] = useState(false);
@@ -20,52 +21,53 @@ const Header = () => {
   
 	  return () => clearTimeout(timer);
 	}, []);
-  return (
-	<>
-		<Navbar />
-		<div className=' relative w-full h-[400px] flex flex-col md:flex-row justify-between items-center md:h-screen bg-cover bg-no-repeat bg-center px-4 py-0 md:py-0'>
-			{/* Left swirl image - hidden on mobile */ }
-			<div className="absolute md:static md:w- md:block md:w-1/4 lg:w-1/3">
-				<img
-					src={Vector4.src} 
-					alt="vector"
-					className="w-3/4 h-auto max-w-xs md:top-2 mx-auto md:mx-0 "
-				/>
-        	</div>
-			<div className="text-center md:w-1/2 z-10 my-8 md:my-0">
-				<h1 className="text-7xl sm:text-7xl md:text-6xl lg:text-7xl xl:text-8xl mb-5 font-bold text-[#000000]">
-					your Daily <br />
-					<span className={`text-7xl text-[#CD4631] sm:text-7xl md:text-6xl lg:text-7xl xl:text-8xl inline-block transition-all duration-1000 transform ${
-                	isVisible ? 'translate-y-0 opacity-100' : 'translate-y-10 opacity-0'
-              		}`}
-					>Podcast</span>
-				</h1>
-				<Button text="Subscribe" onClick={() => console.log('Subscribed!')} />
-        	</div>
-			<div className="absolute right-2 md:static md:flex md:justify-center md:w-1/4 lg:w-1/3 mt-4 md:mb-50 z-10">
-				<img 
-					src={stars.src} 
-					alt="stars"
-					className="w-1/4 h-auto max-w-xs mx-auto md:mx-0 "
-				/>
-        	</div>
-			
-		</div>
-		<div>
-			<EpisodeList />
-		</div>
-		<div className="flex flex-col items-center justify-center py-5 px-4 md:px-10">
-			<hr className="w-full border border-gray-300 m-2" />
-			<div className='flex flex-col md:flex-row items-center justify-center space-x-4 md:space-x-0 md:space-y-4 py-5'>
-				<p className='px-3 py-2'>Supported By:</p>
-				<img className='px-3 py-2' src={spotify.src} alt="spotify" />
-				<img className='px-3 py-2' src={google.src} alt="google" />
-				<img className='px-3 py-2' src={youtube.src} alt="youtube" />
+  	return (
+		<>
+			<Navbar />
+			<div className=' relative w-full h-[400px] flex flex-col md:flex-row justify-between items-center md:h-screen bg-cover bg-no-repeat bg-center px-4 py-0 md:py-0'>
+				{/* Left swirl image - hidden on mobile */ }
+				<div className="absolute md:static md:w- md:block md:w-1/4 lg:w-1/3">
+					<img
+						src={Vector4.src} 
+						alt="vector"
+						className="w-3/4 h-auto max-w-xs md:top-2 mx-auto md:mx-0 "
+					/>
+				</div>
+				<div className="text-center md:w-1/2 z-10 my-8 md:my-0">
+					<h1 className="text-7xl sm:text-7xl md:text-6xl lg:text-7xl xl:text-8xl mb-5 font-bold text-[#000000]">
+						your Daily <br />
+						<span className={`text-7xl text-[#CD4631] sm:text-7xl md:text-6xl lg:text-7xl xl:text-8xl inline-block transition-all duration-1000 transform ${
+						isVisible ? 'translate-y-0 opacity-100' : 'translate-y-10 opacity-0'
+						}`}
+						>Podcast</span>
+					</h1>
+					<Button text="Subscribe" onClick={() => console.log('Subscribed!')} />
+				</div>
+				<div className="absolute right-2 md:static md:flex md:justify-center md:w-1/4 lg:w-1/3 mt-4 md:mb-50 z-10">
+					<img 
+						src={stars.src} 
+						alt="stars"
+						className="w-1/4 h-auto max-w-xs mx-auto md:mx-0 "
+					/>
+				</div>
+				
 			</div>
-			<hr className="w-full border border-gray-300" />
-		</div>
-	</>
-  )
+			<div>
+				<EpisodeList />
+			</div>
+			<div className="flex flex-col items-center justify-center py-5 px-4 md:px-10">
+				<hr className="w-full border border-gray-300 m-2" />
+				<div className='flex flex-col md:flex-row items-center justify-center space-x-4 md:space-x-0 md:space-y-4 py-5'>
+					<p className='px-3 py-2'>Supported By:</p>
+					<img className='px-3 py-2' src={spotify.src} alt="spotify" />
+					<img className='px-3 py-2' src={google.src} alt="google" />
+					<img className='px-3 py-2' src={youtube.src} alt="youtube" />
+				</div>
+				<hr className="w-full border border-gray-300" />
+			</div>
+			<Insipiration />
+		</>
+  	)
 }
 
 export default Header
---
+++ components/layout/Inspiration.tsx
@@ -0,0 +1,86 @@
+'use client';
+import React from 'react';
+import swirl from '../../public/images/scribble.png'
+import illustrator1 from '../../public/images/Illustration-1.svg'
+import swirl2 from '../../public/images/Vector4.png'
+import avatar from '../../public/images/avatar.png'
+import spot from '../../public/images/spot.png'
+import star from '../../public/images/Star4.png'
+import illustrator2 from '../../public/images/Illustration-2.svg'
+
+const Inspiration = () => {
+  return (
+	<>
+		<div className="bg-[#ffffff]">
+			<div className='relative flex w-full bg-[#F7EDE8] min-h-[10px] flex-col items-center mt-2 justify-center p-4 md:flex-row md:justify-between md:p-8'>
+				<img 
+					src={swirl.src}
+					alt="swirl"
+					className="absolute mt-3 h-[100px] max-w-xs mx-auto md:absolute md:top-1/2 md:left-1/2 md:transform md:-translate-x-1/2 md:-translate-y-1/2"
+				/>
+			</div>
+			<div className='relative flex w-full flex-col items-center mt-5 justify-center p-4 md:flex-row md:justify-between md:p-8'>
+				<h1 className='text-4xl sm:text-5xl md:text-6xl lg:text-7xl xl:text-8xl tracking-wider mb-5 font-[700] text-[#000000] text-center'>
+					Talk. Listen. Get inspired <br/>
+					by every minute of it.
+				</h1>
+
+			</div>
+			<div className='flex flex-col md:flex-row items-center justify-between py-5 px-4 md:px-10'>
+				<div className='w-full md:w-1/2 flex flex-col items-center justify-center space-x-4 md:space-x-0 md:space-y-4 py-5'>
+					<img src={illustrator1.src} alt="illustrator1" />
+					<p className='text-center w-4/5 mt-4'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet maxime dolore veritatis sunt, cumque itaque, nulla? Modi magnam ex voluptate consectetur!</p>
+				</div>
+				<div className='flex flex-col w-full md:w-1/2 items-center justify-center space-x-4 md:space-x-0 md:space-y-4 py-5'>
+					<img src={illustrator2.src} alt="illustrator2" />
+					<p className='text-center w-4/5 mt-4'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Minus, sint ut ipsa dolorem enim debitis eos aut consequuntur labore dignissimos vel est neque rerum!</p>
+				</div>
+			</div>
+
+			{/* ---small box----- */}
+
+			<div className='bg-[#ffffff] w-full flex items-center justify-center py-16'>
+				<div className='relative w-full max-w-7xl mx-4 md:mx-8 lg:mx-auto bg-[#F7EDE8] rounded-lg '>
+					{/* Star image - positioned at top-right edge */}
+					<div className='absolute top-0 right-0 transform translate-x-1/4 -translate-y-1/4 md:translate-x-1/3 z-1000'>
+						<img src={star.src} alt="star" className="w-24 md:w-32 lg:w-40" />
+					</div>
+					
+					{/* Swirl image - positioned at left edge */}
+					<div className='absolute top-1/2 left-0 transform -translate-x-1/4 -translate-y-1/2 z-10'>
+						<img src={swirl2.src} alt="swirl" className="w-20 md:w-24 lg:w-32" />
+					</div>
+					
+					{/* Main content */}
+					<div className='flex flex-col md:flex-row items-center justify-between p-6 md:p-12 lg:p-16'>
+						{/* Empty div for spacing where swirl is positioned */}
+						<div className="w-1/12 md:w-1/6"></div>
+					
+					{/* Testimonial content */}
+					<div className='w-full md:w-4/6 flex flex-col items-center justify-center py-8'>
+						<h2 className="text-6xl md:text-7xl lg:text-9xl font-bold text-[#CD4631] leading-none">"</h2>
+						<h2 className="text-lg md:text-xl lg:text-2xl text-center font-medium my-4">
+						One of the best daily podcasts that<br/>covers every topic on Spotify.
+						</h2>
+						
+						{/* Avatar and credentials */}
+						<div className='flex flex-col md:flex-row items-center justify-center w-full mt-6'>
+							<img src={avatar.src} alt="avatar" className="w-16 h-16 rounded-full" />
+							<div className='flex flex-col md:flex-row items-center mt-4 md:mt-0 md:ml-4'>
+								<img className='h-6 mr-2' src={spot.src} alt="spotify" />
+								<h4 className='font-bold text-sm md:text-base'>Social community manager</h4>
+							</div>
+						</div>
+					</div>
+					
+						{/* Empty div for spacing where star extends */}
+						<div className="w-1/12 md:w-1/6"></div>
+					</div>
+				</div>
+			</div>
+      </div>
+	</>
+  );
+};
+
+export default Inspiration;
