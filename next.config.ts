a5f2e51d5d6b546c598a71b387423185d39586bd
lupamo <arnoldlupamo@gmail.com>
2025-05-01 09:46:15 +0300

    config error checks

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,22 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'standalone',
+  distDir: '.next',
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**',
+      },
+    ],
+  },
 };
+
 
 export default nextConfig;
