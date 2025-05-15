b076872ad3225a8f69c2233c0eb3956767be2269
lupamo <arnoldlupamo@gmail.com>
2025-05-15 13:03:53 +0300

    about fixed

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,9 +1,11 @@
 'use client'
 import React, { useState, useEffect, useRef } from 'react'
-import Button from '../../components/ul/Buttons'
+import sparkle from '../../public/images/sparkle-half.png'
 import Vector4 from '../../public/images/shape-swirl.svg'
 import Navbar from '../../components/layout/Navbar'
 import stars from '../../public/images/stars.png'
+import wave from '../../public/images/wave.png'
+import main from '../../public/images/main.png'
 import mic from '../../public/images/mic.png'
 import gsap from 'gsap'
 
@@ -110,25 +112,60 @@ const About = () => {
       {/* ----What Our Listeners say about us---------- */}
       <div className='w-full bg-[#ffffff]'>
         <div className='w-full z-100 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 justify-items-center -mt-6'>
-              <div className='border rounded-lg border-[#000000] w-[80%] max-w-sm h-[200px] flex flex-col justify-center items-center p-4'>
+              <div className='border rounded-lg border-[#000000] w-[80%] max-w-sm h-[200px] flex flex-col justify-center items-center p-4 shadow-[4px_4px_0px_0px_rgba(205,70,49,1)]'>
                 <h2 className='text-[#CD4631] text-4xl font-bold mb-3'>72<span className='text-[#000000]'>K</span></h2>
                 <p>Community Members</p>
               </div>
-              <div className='border rounded-lg border-[#000000] w-[80%] max-w-sm h-[200px] flex flex-col justify-center items-center p-4'>
+              <div className='border rounded-lg border-[#000000] w-[80%] max-w-sm h-[200px] flex flex-col justify-center items-center p-4 shadow-[4px_4px_0px_0px_rgba(205,70,49,1)]'>
                 <h2 className='text-[#CD4631] text-4xl font-bold mb-3'>128<span className='text-[#000000]'>K</span></h2>
                 <p>Subscribers</p>
               </div>
-              <div className='border rounded-lg border-[#000000] w-[80%] max-w-sm h-[200px] flex flex-col justify-center items-center p-4'>
+              <div className='border rounded-lg border-[#000000] w-[80%] max-w-sm h-[200px] flex flex-col justify-center items-center p-4 shadow-[4px_4px_0px_0px_rgba(205,70,49,1)]'>
                 <h2 className='text-[#CD4631] text-4xl font-bold mb-3'>59<span className='text-[#000000]'>K</span></h2>
                 <p>Daily Listeners</p>
               </div>
         </div>
-        <div className='w-full flex flex-col justify-center items-center mt-10'>
-          <h2>What our listeners say</h2>
-          <p>Their experience throughout every platform</p>
-          <div className='w-[80%] flex flex-col justify-center items-center p-4'>
+        <div className='w-full flex flex-col justify-center items-center my-10'>
+          <h2 className='text-3xl sm:text-3xl mb-3 md:text-4xl lg:text-3xl xl:text-4xl tracking-wider font-[700] text-[#000000] text-center mt-6'>What our listeners say</h2>
+          <p className='font-700 text-gray-700 mb-2'>Their experience throughout every platform</p>
+          <div className='relative w-[80%] flex flex-col justify-center items-center p-4'>
             <img src={mic.src} alt="mic" />
+            <img src={wave.src} className='absolute top-1/2 left-1/2 w-1/2 max-w-[300px] h-auto transform -translate-x-3/3 -translate-y-1/2 z-10' alt="wave" />
           </div>
+        </div>
+        {/* -----about and history--------- */}
+        <div className='relative flex flex-col items-center transition-all duration-1000 transform'>
+          {/* Main heading with directly positioned sparkle */}
+          <h4 className='relative text-3xl sm:text-3xl md:text-4xl lg:text-3xl xl:text-4xl tracking-wider font-[700] text-[#000000] text-center mt-6'>
+            {/* Sparkle positioned relative to the text */}
+            <div className="absolute -left-8 -top-8 sm:-left-10 sm:-top-10 md:-left-12 md:-top-12 lg:-left-14 lg:-top-14 w-12 sm:w-16 md:w-20 lg:w-24 z-10">
+              <img src={sparkle.src} alt="spark" className="w-full h-auto" />
+            </div>
+            About and History<br/>
+          </h4>
+          <div className='flex flex-col md:flex-row gap-5 justify-center items-center p-4 m-3'>
+            <p className='leading-relaxed text-[#4D4D4D]'>
+              Eu non diam phasellus vestibulum lorem. Fringilla est ullamcorper eget nulla facilisi etiam dignissim.<br /> Id diam vel quam elementum pulvinar.
+              Elementum eu facilisis sed odio morbi quis commodo. Sed odio morbi quis commodo odio aenean sed adipiscing odio diam. uno fablotoson louw uit.
+            </p>
+            <p className='leading-relaxed text-[#4D4D4D]'>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quam pellentesque at bibendum euismod tellus duis cursus dignissim odiob. Sit vulputate et integer in. <br />
+               Sit vel, senectus iaculis morbi. 
+              Amet interdum imperdiet laoreet morbi tincidunt fermentum, libero. Ante enim eget.
+               Viverra at porttitor accumsan. Orci non here
+            </p>
+            <hr className="border border-gray-400 w-80% my-3"/>
+          </div>
+        </div>
+        {/* ------- Founders-----------*/}
+        <div>
+          <h4 className='relative text-3xl sm:text-3xl md:text-4xl lg:text-3xl xl:text-4xl tracking-wider font-[700] text-[#000000] text-center mt-6'>
+            {/* Sparkle positioned relative to the text */}
+            Founder and Main Host
+            <div className="absolute -left-8 -top-8 sm:-left-10 sm:-top-10 md:left-12 md:-top-12 lg:-left-14 lg:-top-14 w-12 sm:w-16 md:w-20 lg:w-24 z-10">
+              <img src={main.src} alt="spark" className="w-full h-auto" />
+            </div>
+          </h4>
         </div>
       </div>
     </div>
